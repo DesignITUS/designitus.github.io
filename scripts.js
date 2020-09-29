ed01c8c56b108552ee2335ede7411fb264d4c599
Bui Quang Bao <60503568+buiquangbao@users.noreply.github.com>
2020-09-29 17:10:59 +0700

    Update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 
 
 
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+    // document.getElementById("myNav").style.height = "100%";
+    // playPause();
+}
+function closeNav() {
+    // document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav").style.display = "none";
+    // playPause();
+}
